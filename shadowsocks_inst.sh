175d9947b3c6f2173c0e9d8f0b81a9a1e84dc9c7
tivanogemini <brusolekis@outlook.com>
2025-12-15 00:07:21 +0800

    Fix JSON syntax in shadowsocks configuration

--- shadowsocks_inst.sh
+++ shadowsocks_inst.sh
@@ -59,12 +59,12 @@ genConf(){
         sleep 5
         cat<<EOF>/var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
 {
-        server:[::0,0.0.0.0],
-        server_port:$port,
-        password:"$passwd",
-        method:chacha20-ietf-poly1305,
-        mode:tcp_and_udp,
-        fast_open:false
+    "server":["::0","0.0.0.0"],
+    "server_port":$port,
+    "password":"$passwd",
+    "method":"chacha20-ietf-poly1305",
+    "mode":"tcp_and_udp",
+    "fast_open":false
 }
 EOF
 }
